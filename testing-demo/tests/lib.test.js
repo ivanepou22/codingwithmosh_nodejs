a14edc4dab02a333a39ac5cbdbb49d1194e4649d
Ivan Epou <ivanepou92@gmail.com>
2026-04-16 15:04:23 +0300

    Refactor absolute function and group test cases for clarity

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,5 +1,29 @@
 const lib = require('../lib');
-test('absolute - should return a positive number if the input is positive', () => {
-    const result = lib.absolute(5);
-    expect(result).toBe(5);
+// test('absolute - should return a positive number if the input is positive', () => {
+//     const result = lib.absolute(5);
+//     expect(result).toBe(5);
+// });
+
+// test('absolute - should return a positive number if the input is negative', () => {
+//     const result = lib.absolute(-5);
+//     expect(result).toBe(5);
+// });
+// test('absolute - should return a positive number if the input is zero', () => {
+//     const result = lib.absolute(0);
+//     expect(result).toBe(0);
+// });
+
+describe('absolute', () => {
+    it('Should return a positive number if the input is positive', () => {
+        const result = lib.absolute(5);
+        expect(result).toBe(5);
+    });
+    it('Should return a positive number if the input is negative', () => {
+        const result = lib.absolute(-5);
+        expect(result).toBe(5);
+    });
+    it('Should return a positive number if the input is zero', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
