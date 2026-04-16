e68d37672d7b311be90e05b0f1d688ff0449ec30
Ivan Epou <ivanepou92@gmail.com>
2026-04-16 15:15:05 +0300

    Add testing arrays

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -27,3 +27,30 @@ describe('absolute', () => {
         expect(result).toBe(0);
     });
 });
+
+describe('greet', () => {
+    it('Should return the greeting message', () => {
+        const result = lib.greet('Ivan');
+        expect(result).toMatch(/Ivan/);
+    });
+});
+
+describe('getCurrencies', () => {
+    it('Should return supported currencies', () => {
+        const result = lib.getCurrencies();
+        // Too general
+        // expect(result).toBeDefined();
+        // expect(result).not.toBeNull();
+        // Too specific
+        // expect(result[0]).toBe('USD');
+        // expect(result[1]).toBe('AUD');
+        // expect(result[2]).toBe('EUR');
+
+        // // Proper way        expect(result).toContain('USD');
+        // expect(result).toContain('AUD');
+        // expect(result).toContain('EUR');
+
+        // Ideal way
+        expect(result).toEqual(expect.arrayContaining(['USD', 'AUD', 'EUR']));
+    });
+});
