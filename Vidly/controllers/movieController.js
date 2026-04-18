082182f0eeed605926eafb9af6484eee854ba8f9
Ivan Epou <ivanepou92@gmail.com>
2026-04-18 18:56:19 +0300

    Use the asyncMiddleware on the movieController

--- Vidly/controllers/movieController.js
+++ Vidly/controllers/movieController.js
@@ -3,40 +3,29 @@ import { Movie } from "../models/movieModel.js";
 import { Genre } from "../models/genreModel.js";
 import mongoose from "mongoose";
 import { User } from "../models/userModel.js";
+import { asyncMiddleware } from "../middleware/async.js";
 
-export const getMovies = async (req, res) => {
-    try {
-        const query = req.user.isAdmin ? {} : { 'user._id': req.user._id };
-        const movies = await Movie.find(query).sort('title');
-        res.send(movies);
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
-}
+export const getMovies = asyncMiddleware(async (req, res) => {
+    const query = req.user.isAdmin ? {} : { 'user._id': req.user._id };
+    const movies = await Movie.find(query).sort('title');
+    res.send(movies);
+});
 
-export const getMovie = async (req, res) => {
-    try {
-        const query = req.user.isAdmin ? { _id: req.params.id } : { _id: req.params.id, 'user._id': req.user._id };
-        const movie = await Movie.findOne(query);
-        if (!movie) return res.status(404).send('Movie not found');
-        res.send(movie);
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
-}
+export const getMovie = asyncMiddleware(async (req, res) => {
+    const query = req.user.isAdmin ? { _id: req.params.id } : { _id: req.params.id, 'user._id': req.user._id };
+    const movie = await Movie.findOne(query);
+    if (!movie) return res.status(404).send('Movie not found');
+    res.send(movie);
+});
 
-export const deleteMovie = async (req, res) => {
-    try {
-        const query = req.user.isAdmin ? { _id: req.params.id } : { _id: req.params.id, 'user._id': req.user._id };
-        const movie = await Movie.findOneAndDelete(query);
-        if (!movie) return res.status(404).send('Movie not found');
-        res.send(movie);
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
-}
+export const deleteMovie = asyncMiddleware(async (req, res) => {
+    const query = req.user.isAdmin ? { _id: req.params.id } : { _id: req.params.id, 'user._id': req.user._id };
+    const movie = await Movie.findOneAndDelete(query);
+    if (!movie) return res.status(404).send('Movie not found');
+    res.send(movie);
+});
 
-export const updateMovie = async (req, res) => {
+export const updateMovie = asyncMiddleware(async (req, res) => {
     const { error } = validateUpdateMovie(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -56,53 +45,45 @@ export const updateMovie = async (req, res) => {
         };
     }
 
-    try {
-        const query = req.user.isAdmin ? { _id: req.params.id } : { _id: req.params.id, 'user._id': req.user._id };
-        const movie = await Movie.findOneAndUpdate(query,
-            { $set: updateData },
-            { returnDocument: 'after', new: true }
-        );
-        if (!movie) return res.status(404).send('Movie not found');
-        res.send(movie);
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
-}
+    const query = req.user.isAdmin ? { _id: req.params.id } : { _id: req.params.id, 'user._id': req.user._id };
+    const movie = await Movie.findOneAndUpdate(query,
+        { $set: updateData },
+        { returnDocument: 'after', new: true }
+    );
+    if (!movie) return res.status(404).send('Movie not found');
+    res.send(movie);
+});
 
-export const createMovie = async (req, res) => {
+export const createMovie = asyncMiddleware(async (req, res) => {
     const { error } = validateMovie(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    try {
-        const genre = await Genre.findById(req.body.genreId);
-        if (!genre) return res.status(400).send('Invalid genre.');
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
 
-        if (!mongoose.Types.ObjectId.isValid(req.user._id))
-            return res.status(400).send('Invalid userId');
-        const user = await User.findById(req.user._id);
-        if (!user) return res.status(400).send('Invalid user');
+    if (!mongoose.Types.ObjectId.isValid(req.user._id))
+        return res.status(400).send('Invalid userId');
+    const user = await User.findById(req.user._id);
+    if (!user) return res.status(400).send('Invalid user');
 
-        const movie = new Movie({
-            user: {
-                _id: user._id,
-                name: user.name,
-                email: user.email
-            },
-            title: req.body.title,
-            genre: {
-                _id: genre._id,
-                name: genre.name
-            },
-            numberInStock: req.body.numberInStock,
-            dailyRentalRate: req.body.dailyRentalRate
-        });
+    const movie = new Movie({
+        user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        },
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    });
 
-        const savedMovie = await movie.save();
-        res.send(savedMovie);
-    } catch (error) {
-        res.status(500).send('Internal Server Error');
-    }
-}
+    const savedMovie = await movie.save();
+    res.send(savedMovie);
+});
 
 export const validateMovie = (movie) => {
     const schema = Joi.object({
