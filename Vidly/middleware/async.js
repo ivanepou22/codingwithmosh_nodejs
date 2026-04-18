7c787285921a3ea0ed7105a56a44ec74f04f8aaf
Ivan Epou <ivanepou92@gmail.com>
2026-04-18 17:18:37 +0300

    Add the error logging in mongodb

--- Vidly/middleware/async.js
+++ Vidly/middleware/async.js
@@ -1,8 +1,27 @@
+import { logger } from "../startup/logging.js";
+
 export function asyncMiddleware(handler) {
     return async (req, res, next) => {
         try {
             await handler(req, res);
         } catch (error) {
+            await logger(
+                {
+                    messageString: error.message,
+                    additionalInfo: {
+                        error: error,
+                        request: {
+                            user: req.user,
+                            rawHeaders: req.rawHeaders,
+                            reqheader: req.headers,
+                            reqBody: req.body,
+                            reqParam: req.params,
+                            reqQuery: req.query,
+                        }
+                    },
+                    type: 'error',
+                }
+            )
             next(error);
         }
     }
--- Vidly/startup/logging.js
+++ Vidly/startup/logging.js
@@ -1,4 +1,6 @@
 import winston from 'winston';
+import 'winston-mongodb';
+import mongoose from 'mongoose';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -15,5 +17,57 @@ export async function logging() {
     );
     winston.add(new winston.transports.File({ filename: 'logfile.log' }));
     // winston.add(new winston.transports.MongoDB({ db: process.env.MONGODB_URL }));
+}
 
+let options = {
+    db: process.env.MONGODB_URL,
+    Collection: 'logs',
+    capped: false,
+    expireAfterSeconds: 2592000,
+    leaveConnectionOpen: false,
+    storeHost: false,
+    metakey: 'additionalInfo',
+}
+
+const log = winston.createLogger({
+    transports: [
+        new winston.transports.MongoDB(options)
+    ]
+})
+
+export async function logger({ messageString = '', additionalInfo = { error: null, request: null }, type = 'error' }) {
+    try {
+        log[type]({
+            message: messageString,
+            additionalInfo: {
+                error: additionalInfo?.error,
+                request: additionalInfo?.request
+                    ? {
+                        user: additionalInfo?.request?.user,
+                        rawHeaders: additionalInfo?.request?.rawHeaders,
+                        reqheader: additionalInfo?.request?.headers,
+                        reqBody: additionalInfo?.request?.body,
+                        reqParam: additionalInfo?.request?.params,
+                        reqQuery: additionalInfo?.request?.query,
+                    }
+                    : null
+            },
+        });
+    } catch (err) {
+        log.error({
+            message: services.loggerService,
+            additionalInfo: {
+                error: err,
+                request: additionalInfo?.request
+                    ? {
+                        rawHeaders: additionalInfo?.request?.rawHeaders,
+                        reqheader: additionalInfo?.request?.headers,
+                        reqBody: additionalInfo?.request?.body,
+                        reqParam: additionalInfo?.request?.params,
+                        reqQuery: additionalInfo?.request?.query,
+                    }
+                    : null
+            }
+        })
+    }
 }
