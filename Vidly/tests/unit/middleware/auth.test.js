4f46093073b9186f0dbb8484aa3d520f3ecf78f4
Ivan Epou <ivanepou92@gmail.com>
2026-04-23 13:16:44 +0300

    Update .gitignore to include coverage directory and modify test script for coverage reporting; enhance auth middleware tests for user validation

--- Vidly/tests/unit/middleware/auth.test.js
+++ Vidly/tests/unit/middleware/auth.test.js
@@ -1,17 +1,22 @@
 import { jest } from '@jest/globals';
 import { auth } from "../../../middleware/auth";
 import { User } from "../../../models/userModel";
+import mongoose from 'mongoose';
 
 describe('Auth Middleware', () => {
     it('should populate req.user with a payload valid JWT', async () => {
-        const token = new User().generateAuthToken();
+        const user = {
+            _id: new mongoose.Types.ObjectId().toHexString(),
+            isAdmin: true
+        };
+        const token = new User(user).generateAuthToken();
         const req = { header: jest.fn().mockReturnValue(token) };
         const res = {};
         const next = jest.fn();
 
         await auth(req, res, next);
 
-        expect(req.user).toBeDefined();
+        expect(req.user).toMatchObject(user);
     });
 
 });
